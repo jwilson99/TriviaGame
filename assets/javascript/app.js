878df2a6ba58a78665ae0f57a6a4a28c102bac31
Julie W <juliewilsongt@gmail.com>
2017-09-05 19:32:49 -0400

    buttons now iterate through questions and reset timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,18 +11,26 @@ var questions = [
 
     {
         "question":"This is a different question?",
-        "choices":["A","B","C","D"],
-        "answer":"B"
+        "choices":["A2","B2","C2","D2"],
+        "answer":"B2"
+    },
+
+    {
+        "question":"This is the third question?",
+        "choices":["A3","B3","C3","D3"],
+        "answer":"C3"
     }
 ];
 
 var time = 30;
 var countdownthing;
+var questionNumber = 0;
 
 //on window load, hide answer choice buttons, add click event
 window.onload = function() {
     $(".answerbtn").css("display","none");
     $("#start").click(start);
+    $(".answerbtn").click(userAnswer);
     //initializes time left to 30 seconds
     $("#timer").html(time);
 };
@@ -34,14 +42,25 @@ function start() {
     $("#start").css("display","none");
     runCountdown();
 
-    //I'm not sure where I want to put this for loop yet
-    for (i=0, i < questions.length, i++) {
-        $("#question").html(questions[i].question);
-        $("#answer1").html(questions[i].choices[0]);
-        $("#answer2").html(questions[i].choices[1]);
-        $("#answer3").html(questions[i].choices[2]);
-        $("#answer4").html(questions[i].choices[3]);
-    }
+    $("#question").html(questions[questionNumber].question);
+    $("#answer1").html(questions[questionNumber].choices[0]);
+    $("#answer2").html(questions[questionNumber].choices[1]);
+    $("#answer3").html(questions[questionNumber].choices[2]);
+    $("#answer4").html(questions[questionNumber].choices[3]);
+    questionNumber = 1;
+}
+
+function userAnswer () {
+    stop();
+    $("#question").html(questions[questionNumber].question);
+    $("#answer1").html(questions[questionNumber].choices[0]);
+    $("#answer2").html(questions[questionNumber].choices[1]);
+    $("#answer3").html(questions[questionNumber].choices[2]);
+    $("#answer4").html(questions[questionNumber].choices[3]);
+    questionNumber += 1;
+    time = 30;
+    $("#timer").html(time);
+    start();
 }
 
 function runCountdown() {
